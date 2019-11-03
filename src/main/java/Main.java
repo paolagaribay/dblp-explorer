213db9ce682660b83c5b69eec04461010936f760
Paola Garibay <paolagaribay55@gmail.com>
2019-11-02 21:38:11 -0500

    json processing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,39 +19,45 @@ public class Main {
             System.exit(1);
         }
         long start = System.currentTimeMillis();
+
         File f = new File(args[0]);
         String key = args[1];
         int n = Integer.parseInt(args[2]);
+
         List<Article> json = new ArrayList<>();
+        List<String> keyTitles = new ArrayList<>();
+
         JsonFactory jf = new JsonFactory();
         JsonParser parser = jf.createJsonParser(f);
-        while (parser.nextToken() != JsonToken.END_OBJECT) {
-            String token = parser.getCurrentName();
+        JsonToken token = parser.nextToken();
+
+        while (token != JsonToken.END_ARRAY) {
+            String name = parser.getCurrentName();
             Article a = new Article();
-            if ("id".equals(token)) {
-                parser.nextToken();
+            if ("id".equals(name)) {
+                token = parser.nextToken();
                 a.setId(parser.getText());
             }
-            if ("title".equals(token)) {
-                parser.nextToken();
+            if ("title".equals(name)) {
+                token = parser.nextToken();
                 a.setTitle(parser.getText());
             }
-            if ("n_citations".equals(token)) {
-                parser.nextToken();
+            if ("n_citations".equals(name)) {
+                token = parser.nextToken();
                 a.setCitations(Integer.parseInt(parser.getText()));
             }
-            if ("references".equals(token)) {
-                parser.nextToken();
+            if ("references".equals(name)) {
+                token = parser.nextToken();
                 List<String> ref = new ArrayList<>();
                 while (parser.nextToken() != JsonToken.END_ARRAY) {
                     ref.add(parser.getText());
                 }
                 a.setReferences(ref);
             }
-            if (parser.nextToken() == JsonToken.END_OBJECT) {
+            if (token == JsonToken.END_OBJECT) {
                 json.add(a);
-                parser.nextToken();
             }
+            token = parser.nextToken();
         }
 
         try (Stream<String>lines = Files.lines(Paths.get(args[0]), Charset.defaultCharset())) {
