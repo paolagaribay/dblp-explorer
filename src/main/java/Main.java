d782cbc6151e2254d827d696a97fc6c85e014d0c
Paola Garibay <paolagaribay55@gmail.com>
2019-11-03 23:40:48 -0600

    output references

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -16,19 +14,20 @@ import org.json.simple.parser.ParseException;
 
 public class Main {
 
-    public static void main(String [] args) throws IOException {
-        /*if (args.length < 3) {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 3) {
             System.out.println("Error, usage: java Main.java jsonFile keyword int");
             System.exit(1);
-        }*/
+        }
         long start = System.currentTimeMillis();
-        String path = "C:\\Users\\Paola\\Documents\\COSC\\COSC 4353\\a5\\src\\main\\java\\test.txt";//args[0]
-        String key = "system";//args[1]
-        int n = 1;//Integer.parseInt(args[2])
+
+        String path = args[0]; //"C:\\Users\\Paola\\Documents\\COSC\\COSC 4353\\a5\\src\\main\\java\\test.txt"
+        String key = args[1]; //"system"
+        int n = Integer.parseInt(args[2]); //1
+
         JSONParser parser = new JSONParser();
         List<JSONObject> jsonList;
-        List<String> refList = new ArrayList<>();
-        HashMap<String, JSONArray> h = new HashMap<>();
+
         Stream<String>lines = Files.lines(Paths.get(path), Charset.defaultCharset());
         jsonList = lines
                 .map(a-> {
@@ -45,16 +44,11 @@ public class Main {
                 .stream()
                 .filter(a -> a.get("title").toString().toLowerCase().contains(key.toLowerCase()))
                 .collect(Collectors.toList());
+
         System.out.println("Printing titles with keyword "+key+": ");
         keyList.forEach(a->System.out.println(a.get("title").toString()));
-
-        keyList.stream().filter(a->a.get("references")!=null).forEach(a-> getRef(a, refList));
-        //refList.stream().distinct().forEach(a->setHash(a, ));
-
-        if(n==1) {
-            System.out.println("\n"+"Article ids for level 1: ");
-            refList.stream().distinct().forEach(System.out::println);
-        }
+        System.out.println("\n"+"Level "+n+" references: ");
+        references(keyList, jsonList);
 
         long stop = System.currentTimeMillis();
         System.out.println("\n"+"Total time taken in ms: " + (stop-start));
@@ -63,10 +57,31 @@ public class Main {
         long memory = r.totalMemory()-r.freeMemory();
         System.out.println("Memory consumption in bytes: "+ memory);
     }
-    private static void getRef(JSONObject a, List<String> l) {
-        JSONArray j = (JSONArray) a.get("references");
-        for (Object i: j) {
-            l.add(i.toString());
+    private static void references(List<JSONObject> klist, List<JSONObject> jlist) {
+        List<JSONObject> refList = new ArrayList<>();
+        List<String> r = new ArrayList<>();
+
+        for(JSONObject j: klist) {
+            if (j.get("references")!=null) {
+                JSONArray ja = (JSONArray) j.get("references");
+                for(Object i: ja) {
+                    List<JSONObject> o = jlist.stream()
+                            .filter(a -> a.get("id").toString().contentEquals(i.toString()))
+                            .collect(Collectors.toList());
+                    if (!o.isEmpty()) {
+                        refList.addAll(o);
+                    }
+                    else {
+                        r.add(i.toString());
+                    }
+                }
+            }
         }
+        if (!r.isEmpty()) {
+            System.out.println("References not shown in file, displaying ids");
+            r.stream().distinct().forEach(System.out::println);
+        }
+        refList.stream().distinct().sorted(Comparator.comparing(a-> (Integer)a.get("n_citations")))
+                .forEach(a-> System.out.println(a.get("title").toString()+", "+a.get("id").toString()));
     }
 }
