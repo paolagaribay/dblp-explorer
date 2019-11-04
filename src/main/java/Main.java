cd5fc70cce41b87a8b73989c7c75710d3d51ed61
Paola Garibay <paolagaribay55@gmail.com>
2019-11-03 20:26:46 -0600

    print level 1 ids

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,18 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 
 public class Main {
+
     public static void main(String [] args) throws IOException {
         /*if (args.length < 3) {
             System.out.println("Error, usage: java Main.java jsonFile keyword int");
@@ -30,9 +29,9 @@ public class Main {
        // HashMap<String, List<String>> h = new HashMap<>();
         Stream<String>lines = Files.lines(Paths.get("C:\\Users\\Paola\\Documents\\COSC\\COSC 4353\\a5\\src\\main\\java\\test.txt"), Charset.defaultCharset());
         jsonList = lines
-                .map(line-> {
+                .map(a-> {
                     try {
-                        return (JSONObject)parser.parse(line);
+                        return (JSONObject)parser.parse(a);
                     } catch (ParseException e) {
                         e.printStackTrace();
                     }
@@ -44,15 +43,16 @@ public class Main {
                 .stream()
                 .filter(a -> a.get("title").toString().toLowerCase().contains(key.toLowerCase()))
                 .collect(Collectors.toList());
-
+        System.out.println("Printing titles with keyword: "+key);
         keyList.forEach(a->System.out.println(a.get("title").toString()));
+        System.out.println("");
 
-        //keyList.stream().forEach(a-> h.put(a.get("title").toString(), refList.addAll(a.get("references"))));
-        //List<JSONObject> refList = keyList
-          //      .stream()
-            //    .sorted(Comparator.comparing(a ->(Integer)a.get("n_citations")))
-              //  .collect(Collectors.toList());
+        keyList.stream().filter(a->a.get("references")!=null).forEach(a-> getRef(a, refList));
 
+        if(n==1) {
+            System.out.println("Article ids for level 1: ");
+            refList.stream().distinct().forEach(System.out::println);
+        }
 
         long stop = System.currentTimeMillis();
         System.out.println("Total time taken in ms: " + (stop-start));
@@ -60,5 +60,11 @@ public class Main {
         r.gc();
         long memory = r.totalMemory()-r.freeMemory();
         System.out.println("Memory consumption in bytes: "+ memory);
+    }
+    private static void getRef(JSONObject a, List<String> l) {
+        JSONArray j = (JSONArray) a.get("references");
+        for (Object i: j) {
+            l.add(i.toString());
+        }
     }
 }
